b39d3804bcd868c09f123eb1266f98808601c60a
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-07-27 11:07:04 +0530

    Update index.js
    Co-Authored-By: sutharsan  <62829664+sparkz-technology@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@ root.render(
 );
 
 // if you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+//  log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
